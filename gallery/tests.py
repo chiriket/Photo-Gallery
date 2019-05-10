2b285d664d6ecdeb30d2a4807426ceff28f4cbf3
Shirley Keter <shirleyketer@gmail.com>
2019-05-10 14:46:20 +0300

    added image test

--- gallery/tests.py
+++ gallery/tests.py
@@ -39,4 +39,35 @@ class CategoryTestClass(TestCase):
         category = Category.get_category_id(self.sports.id)
         self.assertTrue(category.photo_category == 'SUV')
 
+class ImageTestCase(TestCase):
+    # Set Up Method
+    def setUp(self):
+        self.sports = Category(photo_category='Sports')
+        self.sports.save_category()
 
+        self.nairobi = Location(photo_location='Nairobi')
+        self.nairobi.save_location()
+
+        self.image = Image(name='Lamborghini', description='very first car', location=self.nairobi, category=self.sports)
+        self.image.save_image()
+    
+    def tearDown(self):
+        self.image.delete_image()
+        self.sports.delete_category()
+        self.nairobi.delete_location()
+    
+    def test_get_all_images(self):
+        images = Image.get_all_images()
+        self.assertTrue(len(images)>0)
+    
+    def test_get_image_by_id(self):
+        images = Image.get_image_by_id(self.image.id)
+        self.assertTrue(images == self.image)
+
+    def test_search_image(self):
+        images = Image.search_image('Sports')
+        self.assertTrue(len(images)>0)
+    
+    def test_filter_by_location(self):
+        images = Image.filter_by_location('Nairobi')
+        self.assertTrue(len(images)>0)
