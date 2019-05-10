f8f7a470fbebec65698eb361bdad911d214a9790
Shirley Keter <shirleyketer@gmail.com>
2019-05-10 14:44:05 +0300

    added location tessts

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Location, Category ,Image
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+    # Set Up Method
+    def setUp(self):
+        self.nairobi = Location(photo_location='Nairobi')
+        self.nairobi.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nairobi,Location))
+    
+    def test_updating_location(self):
+        location = Location.get_location_id(self.nairobi.id)
+        location.update_location('Kitengela')
+        location = Location.get_location_id(self.nairobi.id)
+        self.assertTrue(location.photo_location == 'Kitengela')
+    
+    def tearDown(self):
+        self.nairobi.delete_location()
+
