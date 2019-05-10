ea7a68146b0cd131c23ae53159f01650e544c900
Shirley Keter <shirleyketer@gmail.com>
2019-05-10 12:05:49 +0300

    added location

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    photo_location = models.CharField(max_length=50)
+
+    def save_location(self):
+        self.save()
+    
+    def delete_location(self):
+        self.delete()
+    
+    def update_location(self, update):
+        self.photo_location = update
+        self.save()
+    
+    @classmethod
+    def get_location_id(cls, id):
+        locate = Location.objects.get(pk = id)
+        return locate
+
+    def __str__(self):
+        return self.photo_location
--- gallery/views.py
+++ gallery/views.py
@@ -1,28 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
-from .models import Image, Location, Category
+# from .models import Image, Location, Category
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
 
-class Location(models.Model):
-    photo_location = models.CharField(max_length=50)
+# class Location(models.Model):
+#     photo_location = models.CharField(max_length=50)
 
-    def save_location(self):
-        self.save()
+#     def save_location(self):
+#         self.save()
     
-    def delete_location(self):
-        self.delete()
+#     def delete_location(self):
+#         self.delete()
     
-    def update_location(self, update):
-        self.photo_location = update
-        self.save()
+#     def update_location(self, update):
+#         self.photo_location = update
+#         self.save()
     
-    @classmethod
-    def get_location_id(cls, id):
-        locate = Location.objects.get(pk = id)
-        return locate
+    # @classmethod
+    # def get_location_id(cls, id):
+    #     locate = Location.objects.get(pk = id)
+    #     return locate
 
-    def __str__(self):
-        return self.photo_location
+    # def __str__(self):
+    #     return self.photo_location
