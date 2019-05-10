939b00710415410ff93d5861cb5654d269454542
Shirley Keter <shirleyketer@gmail.com>
2019-05-10 11:33:38 +0300

    added gallery

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Image, Location, Category
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
 
+class Location(models.Model):
+    photo_location = models.CharField(max_length=50)
+
+    def save_location(self):
+        self.save()
+    
+    def delete_location(self):
+        self.delete()
+    
+    def update_location(self, update):
+        self.photo_location = update
+        self.save()
+    
+    @classmethod
+    def get_location_id(cls, id):
+        locate = Location.objects.get(pk = id)
+        return locate
+
+    def __str__(self):
+        return self.photo_location
--- photogallery/settings.py
+++ photogallery/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'gallery',
+    'gallery.apps.GalleryConfig',
     'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
